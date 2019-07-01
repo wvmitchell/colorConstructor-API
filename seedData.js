01077af5e44d2039e8020c4b8151c310ccb5be5b
B3an5 <beantheo@gmail.com>
2019-07-01 15:52:16 -0600

    default seedData
    Co-authored-by: Matthew Kaplan <kaplan.matthew.p@gmail.com>

---
+++ seedData.js
@@ -0,0 +1,17 @@
+const projectsData = [
+  {
+    name: "project 1",
+    palettes: [
+      {
+        name: "palette 1",
+        colors: ["#534496", "#d923eb", "#29c346", "#e51662", "#ee8c59"]
+      },
+      {
+        name: "palette 2",
+        colors: ["#534495", "#d953eb", "#25c346", "#e51652", "#ee5c59"]
+      }
+    ]
+  }
+];
+
+module.exports = projectsData;
