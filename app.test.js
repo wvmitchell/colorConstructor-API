a4b2af3b89c05e29d9c34c9f6bcf58d1e6dab065
B3an5 <beantheo@gmail.com>
2019-07-02 13:40:33 -0600

    travis ci setup

---
+++ app.test.js
@@ -0,0 +1,34 @@
+// const request = require("supertest");
+// const environment = process.env.NODE_ENV || "development";
+// const configuration = require("./knexfile")[environment];
+// const database = require("knex")(configuration);
+
+describe("Server", () => {
+  it("should pass a test", () => {
+    expect(true).toEqual(true);
+  });
+  // beforeEach(async () => {
+  //   await database.seed.run();
+  // });
+
+  // describe("init", () => {
+  //   it("should return a 200 status", async () => {
+  //     const res = await request(app).get("/");
+  //     expect(res.status).toBe(200);
+  //   });
+  // });
+
+  // describe("GET /projects", () => {
+  //   it("should return all the projects in the DB", async () => {
+  //     // setup
+  //     const expectedProjects = await database("projects").select();
+
+  //     // execution
+  //     const res = await request(app).get("/projects");
+  //     const result = res.body;
+
+  //     // expectation
+  //     expect(result).toEqual(expectedProjects);
+  //   });
+  // });
+});
